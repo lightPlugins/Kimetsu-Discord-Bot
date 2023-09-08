98e66bce59934a523a0e1427c3ca8198f7d5be97
lightplugins <lightningdesign@outloo.de>
2023-09-08 15:54:23 +0200

    adding debug command

--- src/main/java/de/lightplugins/kimetsu/commands/DebugCommand.java
+++ src/main/java/de/lightplugins/kimetsu/commands/DebugCommand.java
@@ -1,4 +1,32 @@
 package de.lightplugins.kimetsu.commands;
 
-public class DebugCommand {
+import de.lightplugins.kimetsu.enums.OptionDataPath;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+
+public class DebugCommand extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+
+        Dotenv config = Dotenv.configure().load();
+        long channelID = Long.parseLong(config.get("COMMAND_CHANNEL_ID"));
+
+
+        String triggerCommand = event.getName();
+        if(!triggerCommand.equalsIgnoreCase("coins"))  {
+            return;
+        }
+
+
+        OptionMapping subCommand0 = event.getOption(OptionDataPath.COINS_ADD.getName());
+        if(subCommand0 == null) {
+            event.reply("Missing requirements - login name").setEphemeral(true).queue();
+            return;
+        }
+
+
+    }
 }
